4da05d2e31b3121a34b98ffb9d85034fcde1f5e3
Can Kolay <budchirp@gmail.com>
2025-11-25 22:37:17 +0300

    feat: add vibration

--- app/src/main/kotlin/dev/cankolay/wakeup/domain/service/AlarmPlayer.kt
+++ app/src/main/kotlin/dev/cankolay/wakeup/domain/service/AlarmPlayer.kt
@@ -1,8 +1,12 @@
 package dev.cankolay.wakeup.domain.service
 
 import android.content.Context
+import android.media.AudioAttributes
+import android.media.AudioManager
 import android.media.MediaPlayer
 import android.media.RingtoneManager
+import android.os.VibrationEffect
+import android.os.VibratorManager
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
@@ -10,6 +14,8 @@ class AlarmPlayer @Inject constructor(
     @ApplicationContext private val context: Context
 ) {
     private var player: MediaPlayer? = null
+    private val vibrator = (context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager).defaultVibrator
+    private val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
 
     private fun getAlarmUri() =
         RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
@@ -18,12 +24,26 @@ class AlarmPlayer @Inject constructor(
     fun play() {
         stop()
 
-        player = MediaPlayer.create(context, getAlarmUri())
-        player?.apply {
+        val targetVolume = (audioManager.getStreamMaxVolume(AudioManager.STREAM_ALARM) * 0.5f).toInt()
+        audioManager.setStreamVolume(AudioManager.STREAM_ALARM, targetVolume, 0)
+
+        player = MediaPlayer().apply {
+            setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build()
+            )
+
+            setDataSource(context, getAlarmUri())
+
             isLooping = true
 
+            prepare()
             start()
         }
+
+        startVibration()
     }
 
     fun stop() {
@@ -34,5 +54,17 @@ class AlarmPlayer @Inject constructor(
         }
         
         player = null
+        stopVibration()
+    }
+
+    private fun startVibration() {
+        vibrator.vibrate(VibrationEffect.createWaveform(
+            longArrayOf(0, 1000, 1000),
+            0
+        ))
+    }
+
+    private fun stopVibration() {
+        vibrator.cancel()
     }
 }
--- app/src/main/kotlin/dev/cankolay/wakeup/presentation/AppActivity.kt
+++ app/src/main/kotlin/dev/cankolay/wakeup/presentation/AppActivity.kt
@@ -1,9 +1,14 @@
 package dev.cankolay.wakeup.presentation
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import dagger.hilt.android.AndroidEntryPoint
 import dev.cankolay.wakeup.presentation.ui.theme.AppTheme
 import dev.cankolay.wakeup.presentation.ui.view.WakeupView
@@ -15,6 +20,12 @@ class AppActivity : ComponentActivity() {
 
         enableEdgeToEdge()
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.POST_NOTIFICATIONS), 1)
+            }
+        }
+
         setContent {
             AppTheme {
                 WakeupView()
@@ -22,4 +33,3 @@ class AppActivity : ComponentActivity() {
         }
     }
 }
-
